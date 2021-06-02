759702348a1a0e60f481fa6ccb02562b9699395c
yop2yop <diego.aichele@ug.uchile.cl>
2021-06-02 15:49:15 -0300

    adding fitter

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+# mata procesos anteriores (si estaba desde otro lado el server)
+pgrep stream | xargs -r kill
+
+# inicia virtualenv
+source venv/bin/activate
+
+# lanza el proceso con nohup para evitar que se muera al cerrar terminal
+# y manda a background
+nohup streamlit run --server.enableXsrfProtection=false --server.enableCORS false --server.baseUrlPath=parametricmodel parametricmodel.py
+
+nohup streamlit run --server.enableXsrfProtection=false --server.enableCORS false --server.baseUrlPath=parametricfitters fitter.py
+
+
