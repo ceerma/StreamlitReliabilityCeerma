ce588e76d16a03c43aab9c65a1592937a9fbd03f
yop2yop <diego.aichele@ug.uchile.cl>
2021-10-06 14:21:51 -0300

    update index.py

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ hide_streamlit_style = """
 </style>
 
 """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 ## First option
 # PARAMETRIC MODELS
